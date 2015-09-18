c7bdd398022fddcd9eb86bc68e0eb43a53659170
alejandrodob <alejandrodob@gmail.com>
2015-09-18 17:05:08 +0200

    Remove benchmark file

--- src/game_of_life/benchmark.cljs
+++
@@ -1,9 +0,0 @@
-(ns game-of-life.benchmark
-  (:require [game-of-life.core :as gol]))
-
-;;(let [ex-time
-;;      (time (dotimes [n 100]
-;;              (let [state @gol/world-state]
-;;                (gol/paint-cells state)
-;;                (reset! gol/world-state (gol/next-generation state)))))]
-;;  (println ex-time))
